9ea6703b2316d154c594019a21ea32147544b025
Raul Batoon <rocksbatoon@gmail.com>
2018-11-04 12:33:45 -0800

    Refactor top level routes to its own component RoutesTopLevel.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,19 +11,9 @@ import {
   Redirect
 } from 'react-router-dom';
 
-import About from './About';
-import Hello from './Hello';
-import HelloGoodMorning from './HelloGoodMorning';
-import Contact from './Contact';
-import Home from './Home';
-
-
 import MainNav from './MainNav';
-import PrivateRoute from './PrivateRoute';
+import RoutesTopLevel from './RoutesTopLevel';
 
-import Login, { fakeAuth } from './Login'; 
-
-import Admin from './Admin';
 
 class App extends Component {
   render() {
@@ -32,20 +22,11 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <MainNav></MainNav> 
+            <MainNav /> 
 
           </header>
           <main>
-            {/* Routes will go here */}
-            <Switch>
-              <Route exact={true} path="/" component={Home} />
-              <Route path="/about" component={About} />
-              <Route exact path="/hello" component={Hello} />
-              <Route path="/hello/goodmorning" component={HelloGoodMorning} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/login" component={Login} />
-              <PrivateRoute authed={fakeAuth.isAuthenticated} path="/admin" component={Admin} />
-            </Switch>
+            <RoutesTopLevel />
           </main>
         </div>
       </Router>  
---
+++ src/components/RoutesTopLevel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import About from './About';
+import Hello from './Hello';
+import HelloGoodMorning from './HelloGoodMorning';
+import Contact from './Contact';
+import Home from './Home';
+
+import PrivateRoute from './PrivateRoute';
+import Login, { fakeAuth } from './Login'; 
+import Admin from './Admin';
+
+const RoutesTopLevel = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route exact path="/hello" component={Hello} />
+        <Route path="/hello/goodmorning" component={HelloGoodMorning} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute authed={fakeAuth.isAuthenticated} path="/admin" component={Admin} />
+      </Switch>
+    </div>
+
+  );
+}
+export default RoutesTopLevel;
