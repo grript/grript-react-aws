c5c4aa116268ca4f60dad1017a80c993286ae60d
Raul Batoon <rocksbatoon@gmail.com>
2018-11-04 03:14:09 -0800

    Building nested routing

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 
-const Contact = () => {
+const Contact = ({ match }) => {
   return (
       <div className="contact">
-          <h1 className="page-title">Contact Me</h1>
+          <h1 className="page-title">Contact Us</h1>
+          <ul>
+              <li><Link to={`${match.url}/sales`}>Sales</Link></li>
+              <li><Link to={`${match.url}/support`}>Support</Link></li>
+              <li><Link to={`${match.url}/customer-service`}>Customer Service</Link></li>
+          </ul>
+          <div>
+            <Route path={`${match.path}/sales`} render={() => { return <h1>Sales Page</h1> }}/>
+            <Route path={`${match.path}/support`} render={() => { return <h1>Support Page</h1> }}/>
+            <Route path={`${match.path}/customer-service`} render={() => { return <h1>Customer Service Page</h1> }}/>
+          </div>
       </div>
   );
 }
