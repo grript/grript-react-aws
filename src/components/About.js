7b9547bd5a75d37a8b24ae9666c857c5e61d3a43
Raul Batoon <rocksbatoon@gmail.com>
2018-11-04 01:09:06 -0800

    Building route -- About.js

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => {
+  return (
+      <div className="about-us">
+          <h1 className="page-title">About Me</h1>
+      </div>
+  );
+}
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
+// import route Components here
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+import About from './About';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 16.6
-          </a>
-        </header>
-      </div>
+      <Router>      
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <nav className="nav-primary" aria-label="Primary Navigation">
+              <ul>
+                <li><a href="">Hello</a></li>
+                <li><Link to="/about">About</Link></li>
+                <li><a href="">Contact</a></li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            {/* Routes will go here */}
+            <Route path="/about" component={About} />
+          </main>
+        </div>
+      </Router>  
     );
   }
 }
